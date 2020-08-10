2173164bac5a9999b0115f66d6fa60a946198bb2
Sebastian_Orozco <omsebas@gmail.com>
2020-08-10 20:41:48 +0000

    Add file with the function to print to stdout

---
+++ print_func.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
