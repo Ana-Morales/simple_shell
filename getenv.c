fdfc717929eb6ed9bde33a28d8f83981a589a0e9
Sebastian_Orozco <omsebas@gmail.com>
2020-08-11 15:13:36 +0000

    change on the logic of the function

--- getenv.c
+++ getenv.c
@@ -8,37 +8,35 @@
 char *_getenv(const char *name)
 {
 
-	char **env, *env1 = NULL;
+	char **env, *env1= NULL;
 	char *tok;
-	char *env_name_val[500];
 	char *err = "Name not found";
-	int i, j;
+	int i = 0, j = 0;
 
 	env = environ;
-	i = 0;
-	j = 0;
 	while (env[i] != NULL)
 	{
-		env1 = _strdup(env[i]);
+
+		env1 =strdup(env[i]);
 		tok = strtok(env1, "=");
+
 		while (tok != NULL)
 		{
-			env_name_val[j] = tok;
-			j++;
-			tok = strtok(NULL, "=");
+			if(_strcmp(tok, name) == 0)
+			{
+
+				tok = strtok(NULL, "=");
+				return(tok);
+			}
+			else
+			{
+			
+				j++;
+				tok = strtok(NULL, "=");
+				break;
+			}
 		}
-		env_name_val[j] = NULL;
 		i++;
 	}
-	j = 0;
-	while (env_name_val[j] != NULL)
-	{
-		if (_strcmp(env_name_val[j], name) == 0)
-		{
-			return (env_name_val[j + 1]);
-		}
-		j++;
-	}
-
 	return (err);
 }
