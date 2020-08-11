fae0c069371633365e2acca363b4b3cbc8913ec9
Ana-Morales <moralesanaruth@gmail.com>
2020-08-11 15:30:35 +0000

    Add loop for freeing memory

--- getenv.c
+++ getenv.c
@@ -8,35 +8,44 @@
 char *_getenv(const char *name)
 {
 
-	char **env, *env1= NULL;
-	char *tok;
+	char **env, *env1 = NULL;
+	char *tok, *path;
+	char *env_name_val[500], *env2[100];
 	char *err = "Name not found";
-	int i = 0, j = 0;
+	int i, j;
 
 	env = environ;
+	i = 0;
+	j = 0;
 	while (env[i] != NULL)
 	{
-
-		env1 =strdup(env[i]);
+		env1 = _strdup(env[i]);
+		env2[i] = env1;
 		tok = strtok(env1, "=");
-
 		while (tok != NULL)
 		{
-			if(_strcmp(tok, name) == 0)
-			{
-
-				tok = strtok(NULL, "=");
-				return(tok);
-			}
-			else
-			{
-			
-				j++;
-				tok = strtok(NULL, "=");
-				break;
-			}
+			env_name_val[j] = tok;
+			j++;
+			tok = strtok(NULL, "=");
 		}
+		env_name_val[j] = NULL;
 		i++;
+	}
+	j = 0;
+	while (env_name_val[j] != NULL)
+	{
+		if (_strcmp(env_name_val[j], name) == 0)
+		{
+			path = _strdup(env_name_val[j+1]);
+			i = 0;
+			while (env2[i] != NULL)
+			{
+				free(env2[i]);
+				i++;
+			}
+			return (path);
+		}
+		j++;
 	}
 	return (err);
 }
