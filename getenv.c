90f638a7c8eb8c1b16f0f32d001c6ba79d368618
Sebastian_Orozco <omsebas@gmail.com>
2020-08-17 20:18:06 +0000

    Add of setenv and unsetenv functions

--- getenv.c
+++ getenv.c
@@ -47,3 +47,92 @@ char *_getenv(const char *name)
 	}
 	return (err);
 }
+
+/**
+ *_setenv -  set a envirment value to a value
+ *@name: name of the variable to set
+ *@value: value to set for name
+ *@overwrite: flag that allow the overwrite to an existing
+ *enviroment variable, 1 to overwrite, 0 to not overwrite
+ *Return: 0 on sucess, -1 on failure
+ */
+int _setenv(char *name, char *value, int overwrite)
+{
+	char *str1, *tok, **env, *env1 = NULL;
+	int i = 0;
+
+	if (name == NULL || *name == '\0')
+		return (-1);
+	while (name[i] != '\0')
+	{
+		if (name[i] == '=')
+			return (-1);
+		i++;
+	}
+	env = environ;
+	while (env[i] != NULL)
+	{
+		env1 = _strdup(env[i]);
+		tok = strtok(env1, "=");
+		if (_strcmp(name, tok) == 0)
+		{
+			if (overwrite != 0)
+			{
+				str1 = str_concat(name, "=");
+				env[i] = str_concat(str1, value);
+				free(str1);
+				free(env1);
+				return (0);
+			}
+			free(env1);
+			return (0);
+		}
+		free(env1);
+		i++;
+	}
+	str1 = str_concat(name, "=");
+	env[i] = str_concat(str1, value);
+	free(str1);
+	env[i + 1] = NULL;
+	return (0);
+}
+
+/**
+ * _unsetenv - unset a variable on the enviroment
+ *@name: the variable to unset
+ *Return: 0 on sucess, -1 on failure.
+ */
+int _unsetenv(char *name)
+{
+	char *tok, **env, *env1 = NULL;
+	int i = 0;
+
+	if (name == NULL || *name == '\0')
+		return (-1);
+	while (name[i] != '\0')
+	{
+		if (name[i] == '=')
+			return (-1);
+		i++;
+	}
+	env = environ;
+	while (env[i] != NULL)
+	{
+		env1 = _strdup(env[i]);
+		tok = strtok(env1, "=");
+		if (_strcmp(name, tok) == 0)
+		{
+			while (env[i + 1] != NULL)
+			{
+				env[i] = env[i + 1];
+				i++;
+			}
+			env[i] = NULL;
+			free(env1);
+			return (0);
+		}
+		free(env1);
+		i++;
+	}
+	return (0);
+}
